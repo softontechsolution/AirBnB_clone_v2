58966b89a5879b9c7731d4dc3f1e3df576649ed8
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2024-05-01 12:27:59 -0700

    task 5

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+"""This module instantiates an object of class FileStorage and DBstorage"""
+"""Creates a unique storage instance for the application"""
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from os import environ
 
+# use env to store environ var and devide storage method
+env = environ["HBNB_TYPE_STORAGE"]
+if env == 'db':
+    storage = DBStorage()
+    storage.reload()
 
-storage = FileStorage()
-storage.reload()
+else:
+    storage = FileStorage()
+    storage.reload()
--- models/city.py
+++ models/city.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-"""Defines the City class"""
+""" Defines the City class """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """City class is a subclass of BaseModel"""
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ Represents the city class for a MySQL database
+    Attributes:
+        __tablename__ (str): represents the table name, cities
+        name(sqlalchemy String): represents city name (128 characters)
+        state_id(sqlalchemy String): represents City state_id (60 characters)
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
