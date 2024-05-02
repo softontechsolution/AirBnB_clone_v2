72ae80e1d9205809befd562bf9763c5e668b05eb
softontechsolution <softontechsolution@gmail.com>
2024-05-02 03:11:26 +0000

    solved

--- models/place.py
+++ models/place.py
@@ -15,7 +15,7 @@ association_table = Table('place_amenity', Base.metadata,
 
 
 class Place(BaseModel, Base):
-"""Place class is a subclass of BaseModel"""
+	"""Place class is a subclass of BaseModel"""
 	__tablename__ = 'places'
 	city_id = Column(String(60), ForeignKey('cities_id'), nullable=False)
 	user_id = Column(String(60), ForeignKey('users_id'), nullable=False)
@@ -34,20 +34,20 @@ class Place(BaseModel, Base):
 	if getenv('HBNB_TYPE_STORAGE') != 'db':
 		@property
 		def reviews(self):
-		"""getter attribute returns the list of Review instances"""
+			"""getter attribute returns the list of Review instances"""
 			all_reviews = list(models.storage.all(Review).values())
 			review_list = [review for review in all_reviews if review.place_id == self.id]
-		return review_list
+			return review_list
 
 		@property
 		def amenities(self):
-		"""getter attribute returns the list of Amenity instances"""
+			"""getter attribute returns the list of Amenity instances"""
 			all_amenities = list(models.storage.all(Amenity).values())
 			amenity_list = [amenity for amenity in all_amenities if amenity.id in self.amenity_ids]
-		return amenity_list
+			return amenity_list
 
 		@amenities.setter
 		def amenities(self, value):
-		"""setter attribute"""
+			"""setter attribute"""
 			if isinstance(value, Amenity):
 				self.amenity_ids.append(value.id)
