454ba221be8377706c35822ccbeab3776b274476
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2024-05-04 00:06:14 -0700

    task 2

--- models/__init__.py
+++ models/__init__.py
@@ -1,24 +1,6 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage and DBstorage"""
-"""Creates a unique storage instance for the application"""
-from os import environ
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+
 from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 
-
-# use env to store environ var and devide storage method
-env = environ["HBNB_TYPE_STORAGE"]
-if env == 'db':
-    storage = DBStorage()
-    storage.reload()
-
-else:
-    storage = FileStorage()
-    storage.reload()
+storage = FileStorage()
+storage.reload()
--- models/amenity.py
+++ models/amenity.py
@@ -1,24 +1,8 @@
 #!/usr/bin/python3
-"""Defines the Amenity class."""
-from models.base_model import Base
 from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
-from sqlalchemy.orm import relationship
+"""Defines the Amenity class"""
 
 
-class Amenity(BaseModel, Base):
-    """Represents an Amenity for a MySQL database.
-    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Amenities.
-        name (sqlalchemy String): The amenity name.
-        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    places = relationship(
-            "Place",
-            secondary="place_amenity",
-            viewonly=False,
-            back_populates="amenities")
+class Amenity(BaseModel):
+    """Amenity class inherits from BaseModel"""
+    name = ""
