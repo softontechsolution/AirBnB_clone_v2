f492e66aa3e8d4aa5353ec0ed0a443c4263f8967
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2024-05-04 02:19:45 -0700

    fixed pycodestyle

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""
+    This module instantiates an object of class FileStorage and DBstorage
+    Creates a unique storage instance for the application
+"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
-"""This module instantiates an object of class FileStorage and DBstorage"""
-"""Creates a unique storage instance for the application"""
+
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -10,7 +13,9 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+
 from os import environ
+
 
 # use env to store environ var and devide storage method
 env = environ["HBNB_TYPE_STORAGE"]
