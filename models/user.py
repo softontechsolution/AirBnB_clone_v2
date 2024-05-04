9fb07feb8d0a0cfb85ca6ac3a5ce7a14fb060f61
Softon Tech Solution <98369256+softontechsolution@users.noreply.github.com>
2024-05-03 18:05:12 -0700

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines the User class."""
-import models
-from models.base_model import BaseModel, Base
-import sqlalchemy
-from sqlalchemy import Column, String
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
@@ -24,4 +24,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade="all, delete-orphan")
