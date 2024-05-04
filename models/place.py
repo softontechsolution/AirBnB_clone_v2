d32a0101d3eab7c7d4145eb89fbee1c9a402af87
Softon Tech Solution <98369256+softontechsolution@users.noreply.github.com>
2024-05-03 18:07:41 -0700

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-import models
-from models.base_model import BaseModel, Base
-import sqlalchemy
-from sqlalchemy import Column, String, Integer, Float, ForeignKey,
+from models.base_model import BaseModel
 
 """Defines the Place class"""
 
 
-class Place(BaseModel, Base):
+class Place(BaseModel):
     """Place class is a subclass of BaseModel"""
-    __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
